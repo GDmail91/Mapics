a201ef52216be8116284191a29d160fea282afcf
GDmail <GDmail91@gmail.com>
2015-08-25 22:26:27 +0900

    var_dump 수정

--- View/test.php
+++ View/test.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8"/>
 </head>   
 <body>
-<form action="../Controller/follow_list.php" method="POST">
-   <input type="hidden" name="user_id" value="1" />
+<form action="../Controller/map_load.php" method="POST">
+   <input type="hidden" name="user_id" value="2" />
    <input type="hidden" name="map_name" value="하하" />
    <input type="hidden" name="map_locate" value="Jeju"/>
    <input type="hidden" name="map_id" value="5"/>
