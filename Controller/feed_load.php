c330784b11f2cc30311c33293d9857e1781b96c7
GDmail <GDmail91@gmail.com>
2015-08-26 16:50:56 +0900

    <map_load> <feed_load> nickname 나오게 변경

--- Controller/feed_load.php
+++ Controller/feed_load.php
@@ -1,15 +1,23 @@
 <?
 	include '../Class/mapLoad.php';
+	include '../Class/Auth.php';
 
 	$mapLoad = new mapLoad;
+	$auth = new Auth;
 
 	if ($_POST['num'])
 		$maps = $mapLoad->getMapAllCapture($_POST['num']);
 	else	
 		$maps = $mapLoad->getMapAllCapture();
 
+	$map_list[] = null ;
+	foreach ($maps as $map) {
+		$user_info =  $auth->user_info($map['user_id']);
+		array_push($map_list, $map+array('nickname'=>$user_info['nickname']));
+	}
+
 	for ($i=0; $i < count($maps); $i++) { 
-		$mapArray['mapInfo'.$i] = $maps[$i];
+		$mapArray['mapInfo'.$i] = $map_list[$i+1];
 	}
 	echo(urldecode( json_encode ( $mapArray ))) ;
 
--- Controller/map_load.php
+++ Controller/map_load.php
@@ -1,16 +1,25 @@
 <?
 	include '../Class/mapLoad.php';
+	include '../Class/Auth.php';
 
 	$mapLoad = new mapLoad;
+	$auth = new Auth;
 
 	if ($_POST['num']) {
 		$maps = $mapLoad->getMapCaptureById($_POST['user_id'], $_POST['num']);
 	} else {
 		$maps = $mapLoad->getMapCaptureById($_POST['user_id']);
 	}
+	
+	$map_list[] = null ;
+	foreach ($maps as $map) {
+		$user_info =  $auth->user_info($map['user_id']);
+		array_push($map_list, $map+array('nickname'=>$user_info['nickname']));
+	}
+
 
 	for ($i=0; $i < count($maps); $i++) { 
-		$mapArray['mapInfo'.$i] = $maps[$i];
+		$mapArray['mapInfo'.$i] = $map_list[$i+1];
 	}
 	echo urldecode( json_encode ( $mapArray ));
 
