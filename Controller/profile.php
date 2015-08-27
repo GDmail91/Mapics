0428287605a938dfdb8ce55907e9fcf43c63677b
GDmail <GDmail91@gmail.com>
2015-08-27 16:38:01 +0900

    <profile> 에 career  추가

--- Controller/profile.php
+++ Controller/profile.php
@@ -9,6 +9,7 @@
 		'email'=>$user[0]['email'],
 		'phone'=>$user[0]['phone'],
 		'nickname'=>$user[0]['nickname'],
+		'career'=>$user[0]['career'],
 		'user_photo'=>$user[0]['user_photo'],
 		'follower'=>$user[1]['follower'],
 		'following'=>$user[1]['following']
