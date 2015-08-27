5fe5a9a8f7ef60abd35b1b4c2dd8b216a107d004
GDmail <GDmail91@gmail.com>
2015-08-27 16:30:09 +0900

    <profile> 파싱 수정 <feed_load> <map_load> 해시태그 정보 추가

--- Controller/map_load.php
+++ Controller/map_load.php
@@ -1,25 +1,35 @@
 <?
 	include '../Class/mapLoad.php';
 	include '../Class/Auth.php';
+	include '../Class/Hash_tag.php';
 
 	$mapLoad = new mapLoad;
 	$auth = new Auth;
+	$hash_tag = new Hash_tag;
 
+	// num 값이 있는 경우 num값 만큼만 가져옴 (기본값 5)
 	if ($_POST['num']) {
 		$maps = $mapLoad->getMapCaptureById($_POST['user_id'], $_POST['num']);
 	} else {
 		$maps = $mapLoad->getMapCaptureById($_POST['user_id']);
 	}
-	
-	$map_list[] = null ;
+
+	$map_list[] = null ; // 결과 담을 변수
 	foreach ($maps as $map) {
-		$user_info =  $auth->user_info($map['user_id']);
-		array_push($map_list, $map+array('nickname'=>$user_info['nickname']));
+		$user_info =  $auth->user_info($map['user_id']); // 각 맵의 사용자 정보 가져옴
+
+		$tag = $hash_tag->get_tag_id_by_map($map['map_id']);	// 태그 정보
+		foreach ($tag as $tag_id) {
+			$tag_name .= "#".$hash_tag->get_tag_name($tag_id)." ";	// 태그 네임 저장
+		}
+
+		array_push($map_list, $map+array(
+			'nickname'=>$user_info['nickname'], // 사용자 정보에서 nickname 값 가져옴
+			'tag_name'=>$tag_name)); // tag 이름 가져옴
 	}
 
-
 	for ($i=0; $i < count($maps); $i++) { 
-		$mapArray['mapInfo'.$i] = $map_list[$i+1];
+		$mapArray['mapInfo'.$i] = $map_list[$i+1]; // 각 맵정보를 배열에 담음
 	}
 	echo urldecode( json_encode ( $mapArray ));
 
--- Controller/profile.php
+++ Controller/profile.php
@@ -5,6 +5,15 @@
 
 	$user = $Auth->user_info($_POST['user_id']);
 
-	echo urldecode( json_encode ( $user )) ;
+	$result = array(
+		'email'=>$user[0]['email'],
+		'phone'=>$user[0]['phone'],
+		'nickname'=>$user[0]['nickname'],
+		'user_photo'=>$user[0]['user_photo'],
+		'follower'=>$user[1]['follower'],
+		'following'=>$user[1]['following']
+		);
+
+	echo urldecode( json_encode ( $result )) ;
 
 ?>
